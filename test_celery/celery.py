3b9ed3d075b0e7f93f36dae5a861d19267073565
bbischof <bryan.bischof@gmail.com>
2017-05-09 15:41:17 -0700

    first commit, simple rabbitmq-celery prototype

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('test_celery',
-             broker='amqp://jimmy:jimmy123@localhost/jimmy_vhost',
+             broker='amqp://bryan:bryanpass@localhost/bryan_vhost',
              backend='rpc://',
              include=['test_celery.tasks'])
